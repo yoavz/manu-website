62e1d73ee8861217a0bb30d4da042169d0a32657
yoavz <yoavzimmerman@gmail.com>
2014-06-01 15:34:20 -0700

    updated with bootstrap and data structures

--- main.py
+++ main.py
@@ -5,44 +5,54 @@ michael_score = 0
 antonio_score = 0
 wayne_score = 0
 
+players = [
+	{
+		"name": "michael",
+		"score": 0,
+	},
+	{
+		"name": "antonio",
+		"score": 0
+	},
+	{
+		"name": "wayne",
+		"score": 0
+	},
+	{
+		"name": "robin",
+		"score": 0
+	},
+	{
+		"name": "adnan",
+		"score": 0	
+	}
+]
+
 @app.route('/')
 def hello():
-	global michael_score
-	return render_template('index.html', votesm=michael_score, votesa=antonio_score, votesw=wayne_score)
+	return render_template('index.html', data=players)
 
 @app.route('/like', methods=['POST'])
 def like():
-	global michael_score
-	global antonio_score
-	global wayne_score
+	global players
 
 	for r in request.form.getlist('name'):
-		if r == "michael":
-			michael_score += 1
-		if r == "wayne":
-			wayne_score += 1
-		if r == "antonio":
-			antonio_score += 1
-
+		for player in players:
+			if r == player['name']:
+				player['score'] += 1
 
 	return redirect('/')
 
 @app.route('/unlike', methods=['POST'])
 def unlike():
-	global michael_score
-	global antonio_score
-	global wayne_score
+	global players
 
 	for r in request.form.getlist('name'):
-		if r == "michael":
-			if michael_score > 0:
-				michael_score -= 1
-		if r == "wayne":
-			if wayne_score > 0:
-				wayne_score -= 1
-		if r == "antonio":
-			if antonio_score > 0:
-				antonio_score -= 1
+		for player in players:
+			if r == player['name']:
+				player['score'] -= 1
+				if player['score'] < 0:
+					player['score'] = 0
 
 
 	return redirect('/')
